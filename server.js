7d6da6470abb2aefabbe46da3666e18f1cbc4557
MorenoDaniela <[denu.moreno.1990@gmail.com]>
2020-08-30 20:57:38 -0300

    5

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 constexpress = require('express');
 constapp = express();
 constpath = require('path');
-app.use(express.static(__dirname + '/angularapp'));
+app.use(express.static(__dirname + '/ejercicio-clase1'));
 app.listen(process.env.PORT || 8080);
-app.get('/*', function(req, res) {res.sendFile(path.join(__dirname + '/angularapp/index.html'));});
+app.get('/*', function(req, res) {res.sendFile(path.join(__dirname + '/ejercicio-clase1/index.html'));});
 console.log('Build successful!!');
