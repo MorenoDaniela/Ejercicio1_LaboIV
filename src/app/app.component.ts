a5cdcfc9d2998323024208a2491e9ba5a1f8bce2
MorenoDaniela <[denu.moreno.1990@gmail.com]>
2020-09-03 15:12:42 -0300

    almost

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EjercicioClase1';
+  public edadUno: number;
+  public edadDos: number;
+  public suma: number;
+  public promedio: number;
+
+
+
+  public sumar(): void
+  {
+    this.suma = this.edadUno + this.edadDos;
+    this.promedio = this.suma / 2;
+  }
+
+  public limpiar(): void
+  {
+    this.edadUno = 0;
+    this.edadDos = 0;
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
+import { ErrorComponent } from './componentes/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BienvenidoComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
