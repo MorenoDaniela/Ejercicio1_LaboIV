fe03c20c53072ed71a72beba8733f06f25b5a919
MorenoDaniela <[denu.moreno.1990@gmail.com]>
2020-09-18 14:59:14 -0300

    reparo para heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  personasUrl: 'https://api.mocki.io/v1/570c5e5c',
+  firebaseConfig : {
+    apiKey: 'AIzaSyAMEJpe8Lzj10oYvC36MG_WbRSbnXCLwiw',
+    authDomain: 'newapp-2e886.firebaseapp.com',
+    databaseURL: 'https://newapp-2e886.firebaseio.com',
+    projectId: 'newapp-2e886',
+    storageBucket: 'newapp-2e886.appspot.com',
+    messagingSenderId: '456596992413',
+    appId: '1:456596992413:web:115a2ae067f85b1790ff74',
+    measurementId: 'G-9192PFY40T'
+  }
 };
